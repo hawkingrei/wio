7a6296d3dcd4180e9bc971b05b9ba65881cd2b92
Wangweizhen <hawking.rei@gmail.com>
2019-02-01 00:40:42 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use indicatif::ProgressBar;
+use indicatif::ProgressStyle;
+fn main() {
+    let bar = ProgressBar::new(1000);
+    bar.set_style(ProgressStyle::default_bar()
+    .template("{bar:40.cyan/blue} {bytes}")
+    .progress_chars("##-"));
+    for _ in 0..1000 {
+        bar.inc(1);
+        // ...
+    }
+    bar.finish();
+}
